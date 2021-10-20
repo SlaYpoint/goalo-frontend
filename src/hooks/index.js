d4804c536c8b872f98d0b8291ee9588fb9100d86
SlaYpoint <codersaysady@gmail.com>
2021-10-20 12:18:25 +0530

    chore : updated contexts

--- src/hooks/index.js
+++
@@ -1,1 +0,0 @@
-export { useProductData } from './useProductData';
--- src/hooks/useProductData.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { ProductsContext } from '../contexts/ProductsContext';
-
-export const useProductData = () => {
-    return useContext(ProductsContext);
-}
