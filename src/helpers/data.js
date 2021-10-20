123f9480a267f7a6272adafb5e4c8ea0788e5437
SlaYpoint <codersaysady@gmail.com>
2021-10-20 12:25:44 +0530

    chore : updated data

---
+++ src/helpers/data.js
@@ -0,0 +1,17 @@
+export const sortByList = [
+    "Price: Low to High",
+    "Price: High to Low"
+];
+
+export const categories = [
+    "Accessories", "Backpack", "Hoodie", "Jersey", "T-shirt"
+];
+
+export const brands = [
+    "Adidas",
+    "Chelsea F.C",
+    "La Liga",
+    "Manchester United",
+    "Real Madrid",
+    "SportsStuff",
+];
