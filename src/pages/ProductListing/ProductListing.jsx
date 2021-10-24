5660181eb912098c9b9d13a6ee467e5c59bda14a
SlaYpoint <codersaysady@gmail.com>
2021-10-24 13:47:07 +0530

    style : fixed layout

--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -1,11 +1,13 @@
 import { Products, SortByDropDown, FiltersPanel } from './components';
 
+import './ProductListing.css';
+
 export const ProductListing = () => {
     return (
-        <>
+        <div className="products-container">
             <FiltersPanel/>
             <SortByDropDown/>
             <Products />
-        </>
+        </div>
     );
 }
